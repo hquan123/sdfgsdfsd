635001268a7b3b1171df2a556cb63157be8cbb02
“hquan123 <hquan16102002@gmial.com>
2024-06-21 11:37:52 +0700

    Cau1

--- Models/Cau1.cs
+++ Models/Cau1.cs
@@ -1,7 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace baithi15.Models;
-[Table("Cau132")]
+
 public class Cau1{
 
     public int IdNhanvien { get; set; }
